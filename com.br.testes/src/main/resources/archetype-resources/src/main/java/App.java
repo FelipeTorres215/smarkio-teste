34680f3dee1994f711d4b725b7835bd23b7d329c
Felipe Torres <felipe.torres215@outlook.com>
2021-07-29 20:24:01 -0300

    first commit

---
+++ com.br.testes/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $testes.com.br.testes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
